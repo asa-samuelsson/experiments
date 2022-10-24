8e4e67734b3d84c6b95d4acca47d832be9461c34
asa-samuelsson <asa.samuelsson@me.com>
2022-10-24 15:49:43 +0200

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -8,7 +8,7 @@ if (document.readyState === "complete") {
 
 function runExpCode() {
   
-  console.log("script from github");
+  console.log("åsa's script from github");
   
   
   //*** Adding new CSS ***//
